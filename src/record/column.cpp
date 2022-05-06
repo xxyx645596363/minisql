292d1db5a2bea9ee99afdf9e4ad1f3bc439aae6e
DDDDDstar <2643622564@qq.com>
2022-05-06 21:41:19 +0800

    新增2.2 记录与模式中column和schema的部分，未测试，仅作保存

--- src/record/column.cpp
+++ src/record/column.cpp
@@ -26,17 +26,78 @@ Column::Column(const Column *other) : name_(other->name_), type_(other->type_), 
                                       table_ind_(other->table_ind_), nullable_(other->nullable_),
                                       unique_(other->unique_) {}
 
+//wsx_start
+
 uint32_t Column::SerializeTo(char *buf) const {
-  // replace with your code here
-  return 0;
+  MACH_WRITE_UINT32(buf, COLUMN_MAGIC_NUM);//write magic_num
+  buf += sizeof(uint32_t);//update the buf
+
+  MACH_WRITE_STRING(buf, name_);//write the name of the field
+  buf += sizeof(std::string);
+
+  MACH_WRITE_INT32(buf, type_);//write TypeId
+  buf += sizeof(int);
+
+  MACH_WRITE_UINT32(buf, len_);//write the length
+  buf += sizeof(uint32_t);
+
+  MACH_WRITE_UINT32(buf, table_ind_);//write column position in table
+  buf += sizeof(uint32_t);
+
+  MACH_WRITE_TO(bool, buf, nullable_);//write whether the column can be null
+  buf += sizeof(bool);
+
+  MACH_WRITE_TO(bool, buf, unique_);//write whether the column is unique
+  buf += sizeof(bool);
+
+  return GetSerializedSize();
 }
 
 uint32_t Column::GetSerializedSize() const {
-  // replace with your code here
-  return 0;
+  return static_cast<uint32_t>( 3 * sizeof(uint32_t) + sizeof(std::string) + sizeof(int) + 2 * sizeof(bool) );
 }
 
 uint32_t Column::DeserializeFrom(char *buf, Column *&column, MemHeap *heap) {
-  // replace with your code here
-  return 0;
+  if (column != nullptr) {
+    LOG(WARNING) << "Pointer to column is not null in column deserialize." << std::endl;
+  }
+
+  uint32_t column_len, column_index;
+  int column_type;
+  std::string column_name;
+  bool column_nullable, column_unique;
+
+  ASSERT(MACH_READ_FROM(uint32_t, buf) == COLUMN_MAGIC_NUM, "Wrong for MAGIC_NUM.");//check magic_num
+  buf += sizeof(uint32_t);//update the buf
+
+  column_name = MACH_READ_FROM(std::string, buf);//read the name of the field
+  buf += sizeof(std::string);
+
+  column_type = MACH_READ_FROM(int, buf);//read the type
+  buf += sizeof(int);
+
+  column_len = MACH_READ_FROM(uint32_t, buf);//write the length 
+  buf += sizeof(uint32_t);
+
+  column_index = MACH_READ_FROM(uint32_t, buf);
+  buf += sizeof(uint32_t);//update the buf
+
+  column_nullable = MACH_READ_FROM(bool, buf);
+  buf += sizeof(bool);
+
+  column_unique = MACH_READ_FROM(bool, buf);
+  buf += sizeof(bool);
+
+  if (column_type == TypeId::kTypeChar)
+  {
+    column = ALLOC_P(heap, Column)(column_name, column_type, column_len, column_index, column_nullable, column_unique);
+  }
+  else
+  {
+    column = ALLOC_P(heap, Column)(column_name, column_type, column_index, column_nullable, column_unique);
+  }
+
+  return GetSerializedSize();
 }
+
+//wsx_end
--- src/record/schema.cpp
+++ src/record/schema.cpp
@@ -2,15 +2,47 @@
 
 uint32_t Schema::SerializeTo(char *buf) const {
   // replace with your code here
-  return 0;
+  //write the data of schema into buf
+  MACH_WRITE_TO(uint32_t, buf, SCHEMA_MAGIC_NUM);//magic_num
+  buf += sizeof(uint32_t);//update the buf
+  
+  MACH_WRITE_TO(int, buf, columns_.size());//the number of column
+  buf += sizeof(int);
+
+  for (int i = 0; i < columns_.size(); i++)
+  {
+    buf += columns_[i]->SerializeTo(buf);
+  }
+
+  return GetSerializedSize();
 }
 
 uint32_t Schema::GetSerializedSize() const {
-  // replace with your code here
-  return 0;
+  return static_cast<uint32_t>( sizeof(uint32_t) + sizeof(int) + columns_.size() * Column::GetSerializedSize() );
 }
 
 uint32_t Schema::DeserializeFrom(char *buf, Schema *&schema, MemHeap *heap) {
-  // replace with your code here
-  return 0;
+  if (schema != nullptr) {
+    LOG(WARNING) << "Pointer to schema is not null in schema deserialize." << std::endl;
+  }
+
+  int n_column;
+  std::vector<Column *> columns;
+
+  ASSERT(MACH_READ_FROM(uint32_t, buf) == SCHEMA_MAGIC_NUM, "Wrong for MAGIC_NUM.");//check magic_num
+  buf += sizeof(uint32_t);//update the buf
+
+  n_column = MACH_READ_FROM(int, buf);
+  buf += sizeof(int);
+
+  for (int i = 0; i < n_column; i++)
+  {
+    Column * new_column;
+    buf += Column::DeserializeFrom(buf, new_column, heap);
+    columns.push_back(new_column);
+  }
+  
+  schema = ALLOC_P(heap, Schema)(columns);
+
+  return GetSerializedSize();
 }
