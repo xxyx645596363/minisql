ca5ba06d05c2329d9a5ea1bd1f1e59d06982d30e
hypo <3200103592@zju.edu.cn>
2022-05-07 21:06:14 -0700

    lru_replacer

--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -1,23 +1,42 @@
 #include "buffer/lru_replacer.h"
 
 LRUReplacer::LRUReplacer(size_t num_pages) {
-
 }
 
 LRUReplacer::~LRUReplacer() = default;
 
 bool LRUReplacer::Victim(frame_id_t *frame_id) {
-  return false;
+  bool flag;
+  if (this->len > 0) {
+    *frame_id = this->lrulist.back();
+    this->lrumap.erase(*frame_id);
+    this->lrulist.pop_back();
+    this->len--;
+    flag = true;
+  }
+  else {
+      frame_id = NULL;
+      flag = false;
+  }
+  return flag;
 }
 
 void LRUReplacer::Pin(frame_id_t frame_id) {
-
+  if (this->lrumap.find(frame_id) != this->lrumap.end()) {
+    this->lrulist.erase(this->lrumap[frame_id]);
+    this->lrumap.erase(frame_id);
+    this->len--;
+  }
 }
 
 void LRUReplacer::Unpin(frame_id_t frame_id) {
-
+  if (this->lrumap.find(frame_id) == this->lrumap.end()) {
+    this->lrulist.push_front(frame_id);
+    this->lrumap.insert(std::make_pair(frame_id, this->lrulist.begin()));
+    this->len++;
+  }
 }
 
-size_t LRUReplacer::Size() {
-  return 0;
+size_t LRUReplacer::Size() { 
+  return this->len; 
 }
