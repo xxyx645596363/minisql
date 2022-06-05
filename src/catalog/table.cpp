950ec6c8dcf8d88b5a75ab4b68b5873e27415ce5
DDDDDstar <2643622564@qq.com>
2022-06-05 15:42:33 +0800

    为table_info添加主键信息

--- src/catalog/table.cpp
+++ src/catalog/table.cpp
@@ -21,11 +21,11 @@ uint32_t TableMetadata::DeserializeFrom(char *buf, TableMetadata *&table_meta, M
  * @param heap Memory heap passed by TableInfo
  */
 TableMetadata *TableMetadata::Create(table_id_t table_id, std::string table_name,
-                                     page_id_t root_page_id, TableSchema *schema, MemHeap *heap) {
+                                     page_id_t root_page_id, TableSchema *schema, MemHeap *heap, uint32_t prim_idx) {
   // allocate space for table metadata
   void *buf = heap->Allocate(sizeof(TableMetadata));
-  return new(buf)TableMetadata(table_id, table_name, root_page_id, schema);
+  return new(buf)TableMetadata(table_id, table_name, root_page_id, schema, prim_idx);
 }
 
-TableMetadata::TableMetadata(table_id_t table_id, std::string table_name, page_id_t root_page_id, TableSchema *schema)
-        : table_id_(table_id), table_name_(table_name), root_page_id_(root_page_id), schema_(schema) {}
+TableMetadata::TableMetadata(table_id_t table_id, std::string table_name, page_id_t root_page_id, TableSchema *schema, uint32_t prim_idx)
+        : table_id_(table_id), table_name_(table_name), root_page_id_(root_page_id), schema_(schema), prim_idx_(prim_idx) {}
